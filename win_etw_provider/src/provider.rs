620cf2949cd046e2246d163500959e92c3e13847
Danielle Shahmoon <dshahmoon@microsoft.com>
2025-10-19 12:35:23 +0300

    Return the "new" function

--- win_etw_provider/src/provider.rs
+++ win_etw_provider/src/provider.rs
@@ -10,6 +10,21 @@ use core::sync::atomic::{AtomicU8, Ordering::SeqCst};
 #[cfg(target_os = "windows")]
 use win_support::*;
 
+/// Generates a new activity ID.
+///
+/// This function is only implemented on Windows. On other platforms, it will always return `Err`.
+pub fn new_activity_id() -> Result<GUID, Error> {
+    #[cfg(target_os = "windows")]
+    {
+        win_support::new_activity_id()
+    }
+
+    #[cfg(not(target_os = "windows"))]
+    {
+        Err(Error::NotSupported)
+    }
+}
+
 /// Gets the current activity ID.
 ///
 /// This function is only implemented on Windows. On other platforms, it will always return `Err`.
@@ -285,6 +300,21 @@ mod win_support {
                         is_enabled_code
                     );
                 }
+            }
+        }
+    }
+
+    pub fn new_activity_id() -> Result<GUID, Error> {
+        unsafe {
+            let mut guid: winapi::shared::guiddef::GUID = core::mem::zeroed();
+            let error = evntprov::EventActivityIdControl(
+                evntprov::EVENT_ACTIVITY_CTRL_CREATE_ID,
+                &mut guid,
+            );
+            if error == 0 {
+                Ok(guid.into())
+            } else {
+                Err(Error::WindowsError(error))
             }
         }
     }
